f5f9b4df2f9ae64218658cb80a18c5bdcd8f54d2
Xackery <xackery@gmail.com>
2018-04-28 23:17:18 -0700

    added baseline of generator code from past

--- generator_scripts/perl_parse/main.go
+++ generator_scripts/perl_parse/main.go
@@ -2,356 +2,166 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
-	"regexp"
-	"strings"
 
-	"github.com/pkg/errors"
+	"strings"
+	"time"
+	"unicode"
 )
 
-func main() {
-	path := "../../../Server/zone/embparser_api.cpp"
-	err := readFile(path)
-	if err != nil {
-		log.Panicf("Failed to read file: %s", err.Error())
-	}
+//Path defines file related details
+type path struct {
+	Name    string
+	Scope   string
+	Replace string
 }
 
-//API contains the root objects being scanned
+type RootYaml struct {
+	Scopes []*ScopeYaml
+}
+
+type ScopeYaml struct {
+	Name      string
+	Functions []*FuncYaml
+}
+
+type FuncYaml struct {
+	Name     string
+	Summary  string
+	Example  string
+	Argument string
+}
+
+type Functions []*API
+
+func (s Functions) Len() int      { return len(s) }
+func (s Functions) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+type FunctionsByName struct{ Functions }
+
+func (s FunctionsByName) Less(i, j int) bool { return s.Functions[i].Function < s.Functions[j].Function }
+
+type Events []*Event
+
+func (s Events) Len() int      { return len(s) }
+func (s Events) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+type EventsByName struct{ Events }
+
+func (s EventsByName) Less(i, j int) bool { return s.Events[i].Name < s.Events[j].Name }
+
+//API represents an endpoint
 type API struct {
-	Function  string
+	//This is the prefix to a function
+	Object string
+	//This is the raw functionname, e.g. attacknpc in quest::attacknpc()
+	Function string
+	//Summary of function
+	Summary string
+	//Description is pulled from a mapfile
+	Description string
+	//Scope is object type, e.g. quest in quest::attacknpc()
+	Scope string
+	//Return is the return type, e.g. bool, void, etc
+	Return string
+	//Arguments is a list of arguments
 	Arguments []*Argument
 }
 
-//Argument contains the API name, type, and string for a function argument
+//Argument holds details about arguments
 type Argument struct {
+	//Name of argument, e.g. item_id
 	Name string
+	//Type of argument, e.g. int or string
 	Type string
-	API  *API
+	//API holds details about the function the argument is used, mainly for reporting
+	API *API
+	//Is optional?
+	Optional bool
 }
 
-func readFile(path string) (err error) {
-	inFile, err := os.Open(path)
-	if err != nil {
-		err = errors.Wrap(err, "Failed to open file")
-	}
-	defer inFile.Close()
-	scanner := bufio.NewScanner(inFile)
-	scanner.Split(bufio.ScanLines)
+type Event struct {
+	//Name of event, e.g. EVENT_SAY
+	Name string
+	//Arguments is a list of arguments
+	Arguments []*Argument
+}
 
-	arguments := map[string][]*Argument{}
-	functions := []*API{}
-	reg, err := regexp.Compile(`\]+|\[+|\?+|[...]+`)
-	if err != nil {
-		err = errors.Wrap(err, "Failed to compile regex")
+func main() {
+
+	var err error
+	start := time.Now()
+	if err = perlGenerate(); err != nil {
+		log.Fatalf("Error while generating perl: %s", err.Error())
+	}
+
+	log.Println("Finished in", time.Since(start))
+}
+
+func getNoun(function string) string {
+	function = strings.ToLower(function)
+
+	//first, strip any adjectives
+	for k, _ := range adjectives {
+		if strings.Index(function, strings.ToLower(k)) == 0 {
+			function = function[len(k):]
+		}
+	}
+
+	//now figure out noun
+	for k, v := range nouns {
+		if strings.Contains(function, strings.ToLower(k)) {
+			return v
+		}
+	}
+	return ""
+}
+
+func getAdjective(function string) string {
+	function = strings.ToLower(function)
+	for k, v := range adjectives {
+		if strings.Index(function, strings.ToLower(k)) == 0 {
+			return v
+		}
+	}
+	return ""
+}
+
+func splitFunctionParts(function string) (parts []string) {
+	//first, strip any adjectives
+	for k, _ := range adjectives {
+		if strings.Index(strings.ToLower(function), strings.ToLower(k)) == 0 {
+			function = function[len(k):]
+		}
+	}
+
+	//try snake
+	if strings.Contains(function, "_") { //snake notation
+		parts = strings.Split(function, "_")
 		return
 	}
-	regType, err := regexp.Compile(`(unsigned long|long|int32|bool|uint[0-9]+|int|auto|float|unsigned int|char[ \*]).+([. a-zA-Z]+=)`)
-	if err != nil {
-		err = errors.Wrap(err, "Failed to compile type regex")
-		return
+
+	//Split it by uppercase
+	l := 0
+	for s := function; s != ""; s = s[l:] {
+		l = strings.IndexFunc(s[1:], unicode.IsUpper) + 1
+		if l <= 0 {
+			l = len(s)
+		}
+
+		//The 3 conditionals below is my trying to fix the spaced capitalized words
+		if s[:l] == "I" && len(s) > l+1 && s[:l+1] == "ID" {
+			//log.Println("Found ID")
+			l += 2
+		}
+		if s[:l] == "M" && len(s) > l+2 && s[:l+2] == "MP3" {
+			l += 2
+		}
+		if s[:l] == "N" && len(s) > l+2 && s[:l+2] == "NPC" {
+			//log.Println(s[:l+2])
+			l += 3
+		}
+		parts = append(parts, s[:l])
 	}
-
-	lastArguments := []*Argument{}
-	lastAPI := &API{}
-	lineNum := 0
-	for scanner.Scan() {
-		lineNum++
-		key := ""
-		line := scanner.Text()
-		if len(line) < 1 {
-			continue
-		}
-		if len(lastArguments) > 0 { //existing args to parse
-			for i, argument := range lastArguments {
-				key = fmt.Sprintf("ST(%d)", i)
-				if strings.Contains(line, key) {
-					//We found a definition argument line
-					if argument.Type != "" {
-						continue
-					}
-
-					match := regType.FindStringSubmatch(line)
-					if len(match) < 2 {
-						continue
-					}
-
-					//key = `int`
-					//function = line[strings.Index(line, key)+len(key):]
-					newType := ""
-
-					switch v := strings.TrimSpace(match[1]); v {
-					case "int":
-						newType = "int"
-					case "int32":
-						newType = "int"
-					case "float":
-						newType = "float"
-					case "unsigned int":
-						newType = "uint"
-					case "uint32":
-						newType = "uint"
-					case "uint8":
-						newType = "uint"
-					case "uint":
-						newType = "uint"
-					case "bool":
-						newType = "bool"
-					case "uint16":
-						newType = "uint"
-					case "long":
-						newType = "long"
-					case "unsigned long":
-						newType = "unsigned long"
-					case "char":
-						newType = "string"
-					case "auto":
-						//Auto is tricky
-						if strings.Contains(line, "glm::vec4") {
-							newType = "float"
-						}
-
-					default:
-						log.Printf(`Unknown type: "%s" on line %d`, v, lineNum)
-					}
-					//log.Println("Found arg type", newType, "on index", i, argument.Name)
-					lastArguments[i].Type = newType
-				}
-			}
-		}
-
-		function := ""
-
-		argLine := ""
-		args := []string{}
-		//Find line
-		key = `Perl_croak(aTHX_ "Usage:`
-		if strings.Contains(line, key) {
-			function = line[strings.Index(line, key)+len(key):]
-		}
-
-		for _, argument := range lastArguments {
-			arguments[argument.Name] = append(arguments[argument.Name], argument)
-		}
-
-		lastArguments = []*Argument{}
-
-		//Trim off the endings
-		key = `");`
-		if strings.Contains(function, key) {
-			function = function[0:strings.Index(function, key)]
-		}
-		//Strip out the arguments
-		key = `(`
-		if strings.Contains(function, key) {
-			argLine = function[strings.Index(function, key)+len(key):]
-			function = function[0:strings.Index(function, key)]
-			key = `)`
-			if strings.Contains(argLine, key) {
-				argLine = argLine[:strings.Index(argLine, key)]
-			}
-			key = `=`
-			if strings.Contains(argLine, key) {
-				argLine = argLine[:strings.Index(argLine, key)]
-			}
-			argLine = reg.ReplaceAllString(argLine, "")
-		}
-		key = `,`
-		argLine = strings.TrimSpace(argLine)
-
-		if strings.Contains(argLine, key) {
-			args = strings.Split(argLine, key)
-		}
-
-		if len(function) < 1 {
-			continue
-		}
-
-		newArgs := []string{}
-		for j := range args {
-			args[j] = strings.TrimSpace(args[j])
-			if len(args[j]) == 0 {
-				continue
-			}
-			newArgs = append(newArgs, args[j])
-		}
-
-		lastAPI = &API{
-			Function: function,
-		}
-
-		for _, arg := range newArgs {
-			argType, _ := knownTypes[arg]
-			argument := &Argument{
-				Name: arg,
-				Type: argType,
-				API:  lastAPI,
-			}
-			lastArguments = append(lastArguments, argument)
-		}
-		lastAPI.Arguments = lastArguments
-
-		functions = append(functions, lastAPI)
-	}
-
-	foundCount := 0
-	failCount := 0
-	for key, val := range arguments {
-		isMissing := false
-		line := ""
-		line = fmt.Sprintf("%s used by %d functions:", key, len(val))
-		for _, fnc := range val {
-			line += fmt.Sprintf("%s(%s %s), ", fnc.API.Function, fnc.Type, key)
-			if fnc.Type == "" {
-				isMissing = true
-			}
-		}
-		if isMissing {
-			fmt.Println(line)
-			failCount++
-		} else {
-			foundCount++
-		}
-	}
-	log.Println(foundCount, "functions properly identified,", failCount, "have errors")
-
-	line := ""
-	for _, api := range functions {
-		line += fmt.Sprintf("void %s(", strings.TrimSpace(api.Function))
-		for _, argument := range api.Arguments {
-			line += fmt.Sprintf("%s %s, ", argument.Type, argument.Name)
-		}
-		if len(api.Arguments) > 0 {
-			line = line[0 : len(line)-2]
-		}
-		line += ")\n"
-	}
-	fmt.Println(line)
 	return
-}
-
-var knownTypes = map[string]string{
-	"activity_id":               "uint",
-	"alt_mode":                  "bool",
-	"anim_num":                  "int",
-	"best_z":                    "float",
-	"buttons":                   "int",
-	"channel_id":                "int",
-	"char_id":                   "int",
-	"charges":                   "int",
-	"class_id":                  "int",
-	"client_name":               "string",
-	"color":                     "int",
-	"color_id":                  "int",
-	"condition_id":              "int",
-	"copper":                    "int",
-	"count":                     "int",
-	"debug_level":               "int",
-	"decay_time":                "int",
-	"dest_heading":              "float",
-	"dest_x":                    "float",
-	"dest_y":                    "float",
-	"dest_z":                    "float",
-	"distance":                  "int",
-	"door_id":                   "int",
-	"doorid":                    "uint",
-	"duration":                  "int",
-	"effect_id":                 "int",
-	"elite_material_id":         "int",
-	"enforce_level_requirement": "bool",
-	"explore_id":                "uint",
-	"faction_value":             "int",
-	"fade_in":                   "int",
-	"fade_out":                  "int",
-	"fadeout":                   "uint",
-	"firstname":                 "string",
-	"from":                      "string",
-	"gender_id":                 "int",
-	"gold":                      "int",
-	"grid_id":                   "int",
-	"guild_rank_id":             "int",
-	"heading":                   "float",
-	"hero_forge_model_id":       "int",
-	"ignore_quest_update":       "bool",
-	"instance_id":               "int",
-	"int_unused":                "int",
-	"int_value":                 "int",
-	"is_enabled":                "bool",
-	"is_strict":                 "bool",
-	"item_id":                   "int",
-	"key":                       "string",
-	"language_id":               "int",
-	"lastname":                  "string",
-	"leader_name":               "string",
-	"level":                     "int",
-	"link_name":                 "string",
-	"macro_id":                  "int",
-	"max_level":                 "int",
-	"max_x":                     "float",
-	"max_y":                     "float",
-	"max_z":                     "float",
-	"message":                   "string",
-	"milliseconds":              "int",
-	"min_level":                 "int",
-	"min_x":                     "float",
-	"min_y":                     "float",
-	"min_z":                     "float",
-	"name":                      "string",
-	"new_hour":                  "int",
-	"new_min":                   "int",
-	"node1":                     "int",
-	"node2":                     "int",
-	"npc_id":                    "int",
-	"npc_type_id":               "int",
-	"object_type":               "int",
-	"options":                   "int",
-	"platinum":                  "int",
-	"popup_id":                  "int",
-	"priority":                  "int",
-	"quantity":                  "int",
-	"race_id":                   "int",
-	"remove_item":               "bool",
-	"requested_id":              "int",
-	"reset_base":                "bool",
-	"saveguard":                 "bool",
-	"seconds":                   "int",
-	"send_to_world":             "bool",
-	"signal_id":                 "int",
-	"silent":                    "bool",
-	"silver":                    "int",
-	"size":                      "int",
-	"stat_id":                   "int",
-	"str_value":                 "string",
-	"subject":                   "string",
-	"target_enum":               "string",
-	"target_id":                 "int",
-	"task":                      "int",
-	"task_id":                   "uint",
-	"task_id1":                  "int",
-	"task_id10":                 "int",
-	"task_id2":                  "int",
-	"task_set":                  "int",
-	"taskid":                    "int",
-	"taskid1":                   "int",
-	"taskid2":                   "int",
-	"taskid3":                   "int",
-	"taskid4":                   "int",
-	"teleport":                  "int",
-	"temp":                      "int",
-	"texture_id":                "int",
-	"theme_id":                  "int",
-	"update_world":              "int",
-	"updated_time_till_repop":   "uint",
-	"version":                   "int",
-	"wait_ms":                   "int",
-	"window_title":              "string",
-	"x":                         "float",
-	"y":                         "float",
-	"z":                         "float",
-	"zone_id":                   "int",
-	"zone_short":                "string",
-	`task_id%i`:                 "int",
 }
---
+++ generator_scripts/perl_parse/perlmap.go
@@ -0,0 +1,320 @@
+package main
+
+//perlReturnTypes are mapped to identify what sort of return the script does
+var perlReturnTypes = map[string]string{
+	"boolSV(":   "bool",
+	"PUSHu(":    "uint",
+	"PUSHi(":    "int",
+	"sv_setpv(": "string",
+	"PUSHn(":    "double",
+	"XPUSHs":    "array",
+}
+
+//Paths are where every perl file is at
+var perlPaths = []*path{
+	{
+		Name:    "../../../Server/zone/embparser_api.cpp",
+		Scope:   "General",
+		Replace: "quest",
+	},
+	{
+		Name:  "../../../Server/zone/perl_client.cpp",
+		Scope: "Client",
+	},
+	{
+		Name:    "../../../Server/zone/perl_doors.cpp",
+		Scope:   "Doors",
+		Replace: "door",
+	},
+	{
+		Name:  "../../../Server/zone/perl_entity.cpp",
+		Scope: "EntityList",
+	},
+	{
+		Name:  "../../../Server/zone/perl_groups.cpp",
+		Scope: "Group",
+	},
+	{
+		Name:  "../../../Server/zone/perl_hateentry.cpp",
+		Scope: "HateEntry",
+	},
+	{
+		Name:  "../../../Server/zone/perl_mob.cpp",
+		Scope: "Mob",
+	},
+	{
+		Name:  "../../../Server/zone/perl_npc.cpp",
+		Scope: "NPC",
+	},
+	{
+		Name:  "../../../Server/zone/perl_object.cpp",
+		Scope: "Object",
+	},
+	{
+		Name:    "../../../Server/zone/perl_perlpacket.cpp",
+		Scope:   "PerlPacket",
+		Replace: "packet",
+	},
+	{
+		Name:  "../../../Server/zone/perl_player_corpse.cpp",
+		Scope: "Corpse",
+	},
+	{
+		Name:  "../../../Server/zone/perl_QuestItem.cpp",
+		Scope: "QuestItem",
+	},
+	{
+		Name:  "../../../Server/zone/perl_raids.cpp",
+		Scope: "Raid",
+	},
+	{
+		Name:  "../../../Server/zone/embparser.cpp",
+		Scope: "Event",
+	},
+}
+
+//These are known parameter types
+var perlKnownTypes = map[string]string{
+	"activity_id":               "uint",
+	"alt_mode":                  "bool",
+	"anim_num":                  "int",
+	"augment_id":                "int",
+	"best_z":                    "float",
+	"buttons":                   "int",
+	"byte_position":             "uint",
+	"channel_id":                "int",
+	"char_id":                   "int",
+	"charges":                   "int",
+	"class_id":                  "int",
+	"class_name":                "string",
+	"client":                    "client",
+	"client_name":               "string",
+	"color":                     "int",
+	"color_id":                  "int",
+	"condition_id":              "int",
+	"copper":                    "int",
+	"cost":                      "int",
+	"count":                     "int",
+	"debug_level":               "int",
+	"decay_time":                "int",
+	"dest_heading":              "float",
+	"dest_x":                    "float",
+	"dest_y":                    "float",
+	"dest_z":                    "float",
+	"distance":                  "int",
+	"door_id":                   "int",
+	"duration":                  "int",
+	"effect_id":                 "int",
+	"elite_material_id":         "int",
+	"enforce_level_requirement": "bool",
+	"equip_slot_id":             "int",
+	"exp":                       "int",
+	"explore_id":                "uint",
+	"faction_value":             "int",
+	"fade_in":                   "int",
+	"fade_out":                  "int",
+	"fadeout":                   "uint",
+	"firstname":                 "string",
+	"float_value":               "float",
+	"format":                    "string",
+	"from":                      "string",
+	"gender_id":                 "int",
+	"gold":                      "int",
+	"grid_id":                   "int",
+	"group_id":                  "int",
+	"guild_rank_id":             "int",
+	"heading":                   "float",
+	"hero_forge_model_id":       "int",
+	"icon_id":                   "int",
+	"iFromDB":                   "bool",
+	"ignore_quest_update":       "bool",
+	"in_lastname":               "string",
+	"index":                     "int",
+	"instance_id":               "int",
+	"int_penalty":               "int",
+	"int_unused":                "int",
+	"int_value":                 "int",
+	"is_enabled":                "bool",
+	"is_spell":                  "bool",
+	"is_strict":                 "bool",
+	"iSendToSelf":               "int",
+	"item_id":                   "int",
+	"key":                       "string",
+	"language_id":               "int",
+	"lastname":                  "string",
+	"leader_name":               "string",
+	"length":                    "int",
+	"level":                     "int",
+	"link_name":                 "string",
+	"loot_slot":                 "int",
+	"macro_id":                  "int",
+	"max_level":                 "int",
+	"max_x":                     "float",
+	"max_y":                     "float",
+	"max_z":                     "float",
+	"message":                   "string",
+	"milliseconds":              "int",
+	"min_level":                 "int",
+	"min_x":                     "float",
+	"min_y":                     "float",
+	"min_z":                     "float",
+	"mob":                       "mob",
+	"mob_caster":                "mob",
+	"mob_other":                 "mob",
+	"mob_sender":                "mob",
+	"name":                      "string",
+	"new_hour":                  "int",
+	"new_min":                   "int",
+	"node1":                     "int",
+	"node2":                     "int",
+	"npc_id":                    "int",
+	"npc_type_id":               "int",
+	"number":                    "int",
+	"object_id":                 "int",
+	"object_type":               "int",
+	"op_code":                   "string",
+	"options":                   "int",
+	"part13":                    "float",
+	"part19":                    "float",
+	"platinum":                  "int",
+	"popup_id":                  "int",
+	"priority":                  "int",
+	"quantity":                  "int",
+	"race_id":                   "int",
+	"remove_item":               "bool",
+	"requested_id":              "int",
+	"reset_base":                "bool",
+	"reset_state":               "bool",
+	"saveguard":                 "bool",
+	"scale_factor":              "float",
+	"seconds":                   "int",
+	"send_to_world":             "bool",
+	"signal_id":                 "int",
+	"silent":                    "bool",
+	"silver":                    "int",
+	"size":                      "int",
+	"slot":                      "int",
+	"spell_id":                  "int",
+	"stat_id":                   "int",
+	"str_value":                 "string",
+	"subject":                   "string",
+	"target_enum":               "string",
+	"target_id":                 "int",
+	"task":                      "int",
+	"task_id":                   "uint",
+	"task_id1":                  "int",
+	"task_id10":                 "int",
+	"task_id2":                  "int",
+	"task_set":                  "int",
+	"taskid":                    "int",
+	"taskid1":                   "int",
+	"taskid2":                   "int",
+	"taskid3":                   "int",
+	"taskid4":                   "int",
+	"teleport":                  "int",
+	"temp":                      "int",
+	"texture_id":                "int",
+	"theme_id":                  "int",
+	"type":                      "int",
+	"update_world":              "int",
+	"updated_time_till_repop":   "uint",
+	"value":                     "int",
+	"version":                   "int",
+	"wait_ms":                   "int",
+	"window_title":              "string",
+	"x":                         "float",
+	"y":                         "float",
+	"z":                         "float",
+	"zone_id":                   "int",
+	"zone_short":                "string",
+	`task_id%i`:                 "int",
+}
+
+var perlKnownEventArguments = map[string]string{}
+
+var perlKnownEventTypes = map[string]string{
+	"activity_id":         "int",    //", sep.arg[1]);
+	"caster_id":           "int",    //", extradata);
+	"charid":              "int",    //", char_id);
+	"class":               "int",    //", GetClassIDName(mob->GetClass()));
+	"clicker_id":          "int",    //", extradata);
+	"combat_state":        "int",    //", data);
+	"copper":              "int",    //", GetVar("copper." + std::string(itoa(objid))).c_str());
+	"corpse":              "int",    //", sep.arg[2]);
+	"data":                "string", //", "0");
+	"donecount":           "int",    //", sep.arg[0]);
+	"doorid":              "int",    //", data);
+	"env_damage":          "int",    //", sep.arg[0]);
+	"env_damage_type":     "int",    //", sep.arg[1]);
+	"env_final_damage":    "int",    //", sep.arg[2]);
+	"faction":             "int",    //", itoa(fac));
+	"fished_item":         "int",    //", extradata);
+	"foraged_item":        "int",    //", extradata);
+	"gold":                "int",    //", GetVar("gold." + std::string(itoa(objid))).c_str());
+	"grouped":             "int",    //", mob->IsGrouped());
+	"h":                   "int",    //", npcmob->GetHeading() );
+	"hate_state":          "int",    //", data);
+	"hpevent":             "int",    //", "-1");
+	"hpratio":             "int",    //",npcmob->GetHPRatio());
+	"inchpevent":          "int",    //", "-1");
+	"instanceid":          "int",    //", zone->GetInstanceID());
+	"instanceversion":     "int",    //", zone->GetInstanceVersion());
+	"itemid":              "int",    //", extradata);
+	"itemname":            "string", //", item_inst->GetItem()->Name);
+	"killed":              "int",    //", mob->GetNPCTypeID());
+	"killed_npc_id":       "int",    //", sep.arg[4]);
+	"killer_damage":       "int",    //", sep.arg[1]);
+	"killer_id":           "int",    //", sep.arg[0]);
+	"killer_skill":        "int",    //", sep.arg[3]);
+	"killer_spell":        "int",    //", sep.arg[2]);
+	"langid":              "int",    //", "0");
+	"looted_charges":      "int",    //", sep.arg[1]);
+	"looted_id":           "int",    //", sep.arg[0]);
+	"mlevel":              "int",    //", npcmob->GetLevel());
+	"mname":               "string", //", npcmob->GetName());
+	"mobid":               "int",    //", npcmob->GetID());
+	"name":                "string", //", mob->GetName());
+	"objectid":            "int",    //", data);
+	"option":              "int",    //", data);
+	"picked_up_entity_id": "int",    //", extradata);
+	"picked_up_id":        "int",    //", data);
+	"platinum":            "int",    //", GetVar("platinum." + std::string(itoa(objid))).c_str());
+	"popupid":             "int",    //", data);
+	"quantity":            "int",    //", item_inst->IsStackable() ? item_inst->GetCharges() : 1);
+	"race":                "int",    //", GetRaceIDName(mob->GetRace()));
+	"raided":              "int",    //", mob->IsRaidGrouped());
+	"recipe_id":           "int",    //", extradata);
+	"recipe_name":         "string", //", data);
+	"resurrect":           "int",    //", extradata);
+	"signal":              "int",    //", data);
+	"silver":              "int",    //", GetVar("silver." + std::string(itoa(objid))).c_str());
+	"skill_id":            "int",    //", sep.arg[0]);
+	"skill_level":         "int",    //", sep.arg[1]);
+	"slotid":              "int",    //", extradata);
+	"spawned_entity_id":   "int",    //", sep.arg[0]);
+	"spawned_npc_id":      "int",    //", sep.arg[1]);
+	"spell_id":            "int",    //", data);
+	"status":              "int",    //", mob->CastToClient()->Admin());
+	"target_zone_id":      "int",    //", data);
+	"targetid":            "int",    //", npcmob->GetTarget()->GetID());
+	"targetname":          "string", //", npcmob->GetTarget()->GetName());
+	"task_id":             "int",    //", data);
+	"text":                "string", //", data);
+	"timer":               "int",    //", data);
+	"uguild_id":           "int",    //", mob->CastToClient()->GuildID());
+	"uguildrank":          "int",    //", mob->CastToClient()->GuildRank());
+	"ulevel":              "int",    //", mob->GetLevel());
+	"userid":              "int",    //", mob->GetID());
+	"version":             "int",    //", zone->GetInstanceVersion());
+	"wp":                  "int",    //", data);
+	"x":                   "int",    //", npcmob->GetX() );
+	"y":                   "int",    //", npcmob->GetY() );
+	"z":                   "int",    //", npcmob->GetZ() );
+	"zonehour":            "int",    //", eqTime.hour - 1);
+	"zoneid":              "int",    //", zone->GetZoneID());
+	"zoneln":              "string", //", zone->GetLongName());
+	"zonemin":             "int",    //", eqTime.minute);
+	"zonesn":              "string", //", zone->GetShortName());
+	"zonetime":            "int",    //", (eqTime.hour - 1) * 100 + eqTime.minute);
+	"zoneweather":         "int",    //", zone->zone_weather);
+}
